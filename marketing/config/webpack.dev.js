a2e0f4c8d352b3dd5950a5083229db2e096657ec
saadbess <saad@koubeissi.com>
2022-01-06 01:41:24 +0400

    feat: integrated marketing with container using module federation

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,6 +1,8 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const devConfig = {
   mode: "development",
@@ -11,6 +13,15 @@ const devConfig = {
     },
   },
   plugins: [
+    new ModuleFederationPlugin({
+      // 'marketing will be used as a global variable when the script is loaded inside the container'
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      shared: packageJson.dependencies,
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
