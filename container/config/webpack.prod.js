632e42ec641e17ac0cf5639d740747ae4078a269
saadbess <saad@koubeissi.com>
2022-01-06 23:36:40 +0400

    fix: added publicpath

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -2,12 +2,14 @@ const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
+
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
