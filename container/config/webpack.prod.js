2a96eb1cd259ae767c0dd3797e1ad5da674223ca
saadbess <saad@koubeissi.com>
2022-01-06 13:09:11 +0400

    chore: formatting

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -2,7 +2,6 @@ const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
-
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
