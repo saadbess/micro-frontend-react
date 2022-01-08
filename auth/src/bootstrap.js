f57ecb66f40d0413628dc186db496a8326cdae04
saadbess <saad@koubeissi.com>
2022-01-08 23:02:06 +0400

    feat: integrated auth app

--- auth/src/bootstrap.js
+++ auth/src/bootstrap.js
@@ -1,4 +1,4 @@
-console.log("Marketing...");
+console.log("Auth...");
 import React from "react";
 import ReactDOM from "react-dom";
 import { createMemoryHistory, createBrowserHistory } from "history";
@@ -32,7 +32,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 // If we are in development/isolation
 // call mount immediately
 if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector("#_marketing-dev-root");
+  const devRoot = document.querySelector("#_auth-dev-root");
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
